0b19b91b3a41a180f5ea8e2b382f15ec0a88f3f7
likeliu <likeliu@example.com>
2017-10-20 14:53:02 -0500

    microservices example

---
+++ eclipse/spring-microservices-master/03.microservices/zipkin-distributed-tracing-server/src/main/java/com/in28minutes/microservices/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
@@ -0,0 +1,17 @@
+package com.in28minutes.microservices.zipkindistributedtracingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ZipkinDistributedTracingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinDistributedTracingServerApplication.class, args);
+	}
+}
